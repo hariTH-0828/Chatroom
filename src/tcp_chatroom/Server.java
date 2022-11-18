1d464387e503587cacaab3c22030434a91030201
Hariharanrs_0828 <57134303+hariTH-0828@users.noreply.github.com>
2022-11-18 20:34:27 +0530

    Initial commit

---
+++ src/tcp_chatroom/Server.java
@@ -0,0 +1,53 @@
+package tcp_chatroom;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+
+        try {
+
+            while (!serverSocket.isClosed()) {
+
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected!");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void closeServerSocket() {
+
+        try {
+            if(serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(2808)){
+            System.out.println("Server started...\nWaiting for client...");
+            Server server = new Server(serverSocket);
+
+            server.startServer();
+        }
+    }
+
+}
